b9666e9f66996b5882b94a25db06a22aa130809f
brunohlm <brunohlm@gmail.com>
2020-02-03 20:44:07 -0300

    chore(*): melhorando o codigo

--- gabaritos.py
+++ gabaritos.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 # no momento em que executamos a busca por gabaritos diferentes 
 # usamos o arquivo completo com mais de 5 milhões de participantes
@@ -7,11 +6,7 @@ file_name = 'data.csv'
 dados = pd.read_csv(file_name, sep=';', encoding="ISO-8859-1")
 
 # Estamos aqui separando somente a prova de Ciências da Natureza e suas Tecnologias‎
-heads = ['TX_GABARITO_CN', 'CO_PROVA_CN']
-dados = dados[heads]
-
-# Pegando os gabaritos únicos
-gabaritos = dados.groupby(['TX_GABARITO_CN'])
+print(dados.TX_GABARITO_CN.unique())
 
 # Resultado dos gabaritos unicos 
 # ACEDEAEAEBECAADABBDBCEDEDDCDDABCCACBCACEBDEBB   450
